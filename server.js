f35c84bf1b9056adda8e4e46385ad556fa22b47c
Matthew Beebe <matthewbeebe@Matthews-MacBook-Pro.local>
2017-10-27 13:21:03 -0400

    Fixed scrape errors

--- server.js
+++ server.js
@@ -99,21 +99,19 @@ app.get("/scrape", function(req, res) {
           res.send("Scrape Complete");
         })
         .catch(function(err) {
-
           if (err) {
             return res.send();
           }
           // If an error occurred, send it to the client
           res.json("Scrape Complete");
         });
-        res.redirect("/");
     });
   });
 });
 
 // Route for getting saving Articles from the db
 app.get("/saved", function(req, res) {
-  db.Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
+  db.Article.find({saved: true}, null, {sort: {created: -1}}, function(err, data) {
     if(data.length === 0) {
       res.render("placeholder", {message: "You have not saved any articles yet. Try to save some delicious news by simply clicking \"Save Article\"!"});
     }
@@ -126,12 +124,12 @@ app.get("/saved", function(req, res) {
 app.post("/saved/:id", function(req, res) {
   db.Article.findById(req.params.id, function(err, data) {
     if (data.issaved) {
-      db.Article.findByIdAndUpdate(req.params.id, {$set: {issaved: false, status: "Save Article"}}, {new: true}, function(err, data) {
+      db.Article.findByIdAndUpdate(req.params.id, {$set: {saved: false, status: "Save Article"}}, {new: true}, function(err, data) {
         res.redirect("/");
       });
     }
     else {
-      db.Article.findByIdAndUpdate(req.params.id, {$set: {issaved: true, status: "Saved"}}, {new: true}, function(err, data) {
+      db.Article.findByIdAndUpdate(req.params.id, {$set: {saved: true, status: "Saved"}}, {new: true}, function(err, data) {
         res.redirect("/saved");
       });
     }
@@ -139,42 +137,42 @@ app.post("/saved/:id", function(req, res) {
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article
-    .findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
-    .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+// app.get("/articles/:id", function(req, res) {
+//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+//   db.Article
+//     .findOne({ _id: req.params.id })
+//     // ..and populate all of the notes associated with it
+//     .populate("note")
+//     .then(function(dbArticle) {
+//       // If we were able to successfully find an Article with the given id, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.Note
-    .create(req.body)
-    .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-    })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
+// // Route for saving/updating an Article's associated Note
+// app.post("/articles/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   db.Note
+//     .create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
 
 // Start the server
 app.listen(PORT, function() {
